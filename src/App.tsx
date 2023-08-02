047be166b4635343130f4b41da8e4c8cf2ee769b
Jennifer Fu <jennifer.fu@dominodatalab.com>
2023-08-01 23:33:21 -0700

    Initialize the Vite project for React Router

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  useLocation,
+  useRoutes,
+} from 'react-router-dom';
+import { MainPage } from './MainPage';
+import { PageOne, PageTwo } from './Pages';
+
+function App() {
+  const location = useLocation();
+  useEffect(() => {
+    console.log('Current location is', location);
+  }, [location]);
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <MainPage />,
+      children: [
+        { index: true, element: <div>No page is selected</div> },
+        { path: '*', element: <PageOne /> },
+        {
+          path: 'one',
+          element: <PageOne />,
+          children: [{ path: ':id', element: <PageOne /> }],
+        },
+        {
+          path: 'two',
+          element: <PageTwo />,
+          children: [{ path: ':id', element: <PageTwo /> }],
+        },
+      ],
+    },
+  ]);
+  return routes;
+}
+
+const AppWrapper = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+export default AppWrapper;
---
+++ src/Pages.tsx
@@ -0,0 +1,16 @@
+import { useParams } from 'react-router-dom';
+import { loremIpsum } from 'lorem-ipsum';
+
+const BuildPage = ({index}: {index: number}) => {
+  const { id } = useParams();
+  return (
+    <>
+      <h3>Page {index}</h3>
+      <div>
+      Page {index} {id && <span> - paragraph {id}</span>} content: {loremIpsum({ count: 5 })}
+      </div>
+    </>
+  );
+};
+export const PageOne = () => BuildPage({index: 1});
+export const PageTwo = () => BuildPage({index: 2});
